0c4174765758adeb3ed29ec48639af9691dd2257
Khoi Nguyen <khoi@nguyen.me.uk>
2026-03-07 12:57:14 +0100

    Improve spinner

--- packages/components/src/Spinner.tsx
+++ packages/components/src/Spinner.tsx
@@ -5,9 +5,9 @@ export default function Spinner(props: { fallback?: JSX.Element; children: JSX.E
   return (
     <Suspense
       fallback={
-        <>
-          <img src={spinner} /> {props.fallback}
-        </>
+        <div class="flex items-center gap-8">
+          <img src={spinner} /> <div>{props.fallback}</div>
+        </div>
       }
     >
       {props.children}
