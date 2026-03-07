d08cc04ff2f4e43fd281cec94e6c3a4f6e35449e
Khoi Nguyen <khoi@nguyen.me.uk>
2026-03-07 18:04:03 +0100

    Add an error boundary

--- packages/components/src/Spinner.tsx
+++ packages/components/src/Spinner.tsx
@@ -1,16 +1,27 @@
-import { type JSX, Suspense } from 'solid-js'
+import { ErrorBoundary, type JSX, Suspense } from 'solid-js'
+import Button from './Button'
 import spinner from './Spinner.svg'
 
 export default function Spinner(props: { fallback?: JSX.Element; children: JSX.Element }) {
   return (
-    <Suspense
-      fallback={
-        <div class="flex items-center gap-8">
-          <img src={spinner} /> <div>{props.fallback}</div>
+    <ErrorBoundary
+      fallback={(error, retry) => (
+        <div>
+          <p>Une erreur s'est produite</p>
+          <pre>{error}</pre>
+          <Button onclick={retry}>Réessayer</Button>
         </div>
-      }
+      )}
     >
-      {props.children}
-    </Suspense>
+      <Suspense
+        fallback={
+          <div class="flex items-center gap-8">
+            <img src={spinner} /> <div>{props.fallback}</div>
+          </div>
+        }
+      >
+        {props.children}
+      </Suspense>
+    </ErrorBoundary>
   )
 }
