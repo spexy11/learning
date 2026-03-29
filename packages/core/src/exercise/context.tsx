a1ef364a0f073c32b689a60d62ad1bf31cd7a4fc
Khoi Nguyen <khoi@nguyen.me.uk>
2026-03-29 14:23:20 +0200

    Apply transformations lazily for math fields

--- packages/core/src/exercise/context.tsx
+++ packages/core/src/exercise/context.tsx
@@ -1,6 +1,7 @@
 import { createContext, createStore, useContext } from 'solid-js'
 
 type ContextType = {
+  readOnly?: boolean
   state: Record<string, any>
   setState: ReturnType<typeof createStore<Record<string, any>>>[1]
 }
